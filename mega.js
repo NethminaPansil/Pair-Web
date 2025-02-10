bacd7f0f03231f56bda22e19ce9a7225e1f56ccc
NethminaPansil <nethminapansilu5@gmail.com>
2025-02-10 18:51:56 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "mevawil495@suggets.com",
-  password: "2008.05.03@Pansilu",
+  email: "nekoyok643@zapany.com",
+  password: "2008.05.03@pansilu",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
